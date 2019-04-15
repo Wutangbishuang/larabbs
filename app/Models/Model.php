14fb0c37784f060b16cc10f848c94169a87d2643
Richard <6692223@qq.com>
2019-04-15 03:50:58 +0000

    List user replies

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 class Model extends EloquentModel
 {
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+    
     public function scopeRecent($query)
     {
         return $query->orderBy('id', 'desc');
